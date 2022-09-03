151e379117a5566797114aa742a3d9928e031156
Pavel Usachev <pausachev@gmail.com>
2022-09-04 01:50:08 +0300

    added Array Factory

--- Module1HW4/Program.cs
+++ Module1HW4/Program.cs
@@ -1,4 +1,36 @@
-﻿do
+﻿// <copyright file="Program.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+using Module1HW4.Factory;
+
+do
 {
+    Console.WriteLine("Hello usser");
+    Console.WriteLine("Please enter number of array size: ");
+
+    string usrInput = Console.ReadLine() ?? " ";
+
+    if (usrInput.Trim(' ').Length == 0)
+    {
+        Console.WriteLine("Error: Empty input");
+        continue;
+    }
+
+    int arraySize = 0;
+
+    try
+    {
+        arraySize = int.Parse(usrInput);
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("Plese enter valid number format");
+        continue;
+    }
+
+    int[] array = RandomArrayFactory.Create(arraySize);
+
     Console.WriteLine("Pres Esc to exit or Enter to contilue.");
-} while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+}
+while (Console.ReadKey(true).Key != ConsoleKey.Escape);
